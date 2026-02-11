860831aa2b194a3f9c54e653e2a392ba909d9cbf
Ambi Jayaprakash <118501633+Jay0906@users.noreply.github.com>
2026-02-11 23:51:55 +0530

    Create Program.cs

---
+++ Day 20/M1 Mock/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace M1MockPrac
+{
+    public class Jewellery
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Material { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class JewelleryUtility
+    {
+        public void getJewellery()
+        {
+            
+        }
+
+        public void UpdateJewellery()
+        {
+            
+        }
+    }
+    
+    public class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
